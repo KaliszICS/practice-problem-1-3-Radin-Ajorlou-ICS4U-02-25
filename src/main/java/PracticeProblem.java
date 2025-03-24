31af8b289ed736124a1430c93614720d1131d245
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-03-24 22:19:00 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,107 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.NoSuchFileException;
+
+
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+
+
+
+
+
+	public static String readFile(String fileName){
+
+		BufferedReader inputStream = null;
+		StringBuilder total = new StringBuilder ("");
+		String output = "";
+
+		
+		try
+		{
+			inputStream = new BufferedReader(new FileReader(fileName));
+			String line;
+			while ((line = inputStream.readLine()) != null)
+			{
+				total = total.append(line).append("\n");
+			}
+			output = total.toString();
+		}
+		catch(NoSuchFileException e)
+		{
+			System.out.println(e);
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+		finally
+		{
+			try
+			{
+				if (inputStream != null)
+				{
+					inputStream.close();
+				}
+			}
+			catch (IOException e )
+			{
+				System.out.println(e);
+			}
+		}
+			
+		return output;
+
+
+
+	
+	}
+
+
+
+
+	public static String backwardsReadFile(String fileName){
+		FileReader inputStream = null;
+		StringBuilder fwds = new StringBuilder();
+		try {
+			inputStream = new FileReader(fileName);
+			int s;
+			while ((s = inputStream.read()) != -1){
+				char b = (char)s;
+				Character c = b;
+				String d = c.toString();
+				fwds.append(d);
+			}
+			
+
+			
+		}
+		catch(FileNotFoundException e){
+			System.out.println(e);
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally {
+			try {
+		 		if (inputStream != null) {
+		 			inputStream.close();
+		 		}
+
+		 	}
+		 	catch (IOException e) {
+		 		System.out.println(e);
+		 	}
+			
+		}
+		return fwds.reverse().toString();
+
 	
 
+	}
 }
